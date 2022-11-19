444f1f53d2a3f41ac4a57a7a98c01f3daf15f0cb
Alessandro Sanvito <alessandro.sanvito@gmail.com>
2022-11-19 11:31:37 +0100

    chore: checkpoint train.py

--- recsys/train.py
+++ recsys/train.py
@@ -1,12 +1,32 @@
+import pandas as pd
 from lenskit.algorithms import item_knn, Recommender
-from lenskit.datasets import MovieLens
 
-if __name__=='__main__':
-    dataset = MovieLens()
-    print('Data loaded!')
+from utils.datasets import datasets
+
+
+def train(dataset_name: str, save_path: str = None, verbose: bool = True):
+    if verbose:
+        print('Loading the data...')
+    dataset = datasets[dataset_name]()
+    if verbose:
+        print('Data loaded!')
+
+    print(dataset.ratings)
     recommender = Recommender.adapt(item_knn.ItemItem(20))
 
     urm = dataset.ratings[['user', 'item', 'rating']]
     recommender.fit(urm)
 
+    print(recommender.recommend(1, n=10, ratings=pd.Series(data=[5, 5], index=[0, 1])))
+
+    print()
+
     print('Recommender fitted!')
+
+    if save_path:
+        if verbose:
+            print()
+
+
+if __name__ == '__main__':
+    train('1M')
