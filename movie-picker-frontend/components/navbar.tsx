4e146c5ba973572202e357d81da8636f939789c6
thuanystuart <thuanystuart@gmail.com>
2022-11-19 05:41:50 +0100

    frontend: add search bar

--- movie-picker-frontend/components/navbar.tsx
+++ movie-picker-frontend/components/navbar.tsx
@@ -1,9 +1,12 @@
+import SearchInput from "./search-input"
+
 export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 right-0 h-12 bg-white shadow-lg flex items-center px-10">
       <div className="w-44">
         <img src="/streampicker_logo.svg" alt="StreamPicker - Streaming Guide"></img>
       </div>
+      <SearchInput />
     </div>
   )
 }
