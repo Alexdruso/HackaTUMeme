5c53be0eb1bd7d7c5332972326fc8b2cf2019859
Alessandro Sanvito <alessandro.sanvito@gmail.com>
2022-11-19 15:45:48 +0100

    feat: training algorithm

--- recsys/train.py
+++ recsys/train.py
@@ -1,5 +1,8 @@
+import os.path
+
 import pandas as pd
 from lenskit.algorithms import item_knn, Recommender
+from binpickle import dump
 
 from utils.datasets import datasets
 
@@ -17,16 +20,12 @@ def train(dataset_name: str, save_path: str = None, verbose: bool = True):
     urm = dataset.ratings[['user', 'item', 'rating']]
     recommender.fit(urm)
 
-    print(recommender.recommend(1, n=10, ratings=pd.Series(data=[5, 5], index=[0, 1])))
-
-    print()
-
     print('Recommender fitted!')
 
     if save_path:
-        if verbose:
-            print()
+        if verbose: print('Saving the model to file...')
+        dump(recommender, os.path.join(save_path, 'model.bpk'))
 
 
 if __name__ == '__main__':
-    train('1M')
+    train('1M', save_path='saved_recommenders')
