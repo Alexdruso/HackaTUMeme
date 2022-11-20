58f42318ca31548c19a6563f866b08ae9c30ec95
thuanystuart <thuanystuart@gmail.com>
2022-11-20 10:46:19 +0100

    frontend: add user images to feed

--- movie-picker-frontend/components/notifications/comment.tsx
+++ movie-picker-frontend/components/notifications/comment.tsx
@@ -4,12 +4,12 @@ import UserImage from "../user-image"
 function CommentNotification({ comment, movie, user }: { comment: string, movie: string, user: User | undefined}) {
   return (
     <div className="shadow rounded p-4 flex gap-4">
-      <UserImage size="l" hover={false}></UserImage>
+      <UserImage size="l" hover={false} img={user?.picture}></UserImage>
       <div className="flex flex-col">
         <div>
-          <span className="text-purple font-bold">{user?.first_name}</span>
+          <span className="text-purple font-bold" role="button">{user?.first_name}</span>
           <span className="text-grey"> has commented on movie
-            <span className="text-purple font-bold"> {movie}</span>
+            <span className="text-purple font-bold" role="button"> {movie}</span>
           </span>
         </div>
         <span className="text-grey">{comment}</span>
--- movie-picker-frontend/components/notifications/rated.tsx
+++ movie-picker-frontend/components/notifications/rated.tsx
@@ -4,12 +4,12 @@ import UserImage from "../user-image"
 function RatedNotification({ rating, movie, user }: { rating: number, movie: string, user: User | undefined}) {
   return (
     <div className="shadow rounded p-4 flex gap-4">
-      <UserImage size="l" hover={false}></UserImage>
+      <UserImage size="l" hover={false} img={user?.picture}></UserImage>
       <div>
         <div>
-          <span className="text-purple font-bold">{user?.first_name}</span>
+          <span className="text-purple font-bold" role="button">{user?.first_name}</span>
           <span className="text-grey"> has rated the movie
-            <span className="text-purple font-bold"> {movie} </span>
+            <span className="text-purple font-bold" role="button"> {movie} </span>
             with {rating} stars!
           </span>
         </div>
