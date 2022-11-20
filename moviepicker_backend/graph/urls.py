64877ec1a48aa357ae49ce94be9e5308339f1c7e
Nicolò Sonnino <swagpino@gmail.com>
2022-11-20 03:33:00 +0100

    feat: get_info and duplicate

--- moviepicker_backend/graph/urls.py
+++ moviepicker_backend/graph/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('getMovie/', views.get_node, name='get_node'),
     path('getAll/', views.get_all, name='get_all'),
+    path('getInfo/', views.get_info, name='get_info'),
     # path('login/', views.login_view, name='login'),
 ]
