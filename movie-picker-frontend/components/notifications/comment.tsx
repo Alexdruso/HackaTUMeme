f7604cf614c90af2c9a63aa9907941e06d057c08
thuanystuart <thuanystuart@gmail.com>
2022-11-20 10:42:52 +0100

    frontend: add personalized feed

--- movie-picker-frontend/components/notifications/comment.tsx
+++ movie-picker-frontend/components/notifications/comment.tsx
@@ -1,13 +1,16 @@
+import { User } from "../../models/user"
 import UserImage from "../user-image"
 
-function CommentNotification({ comment }: { comment: string}) {
+function CommentNotification({ comment, movie, user }: { comment: string, movie: string, user: User | undefined}) {
   return (
     <div className="shadow rounded p-4 flex gap-4">
       <UserImage size="l" hover={false}></UserImage>
       <div className="flex flex-col">
         <div>
-          <span className="text-purple font-bold">User name</span>
-          <span className="text-grey"> has commented on movie blabla</span>
+          <span className="text-purple font-bold">{user?.first_name}</span>
+          <span className="text-grey"> has commented on movie
+            <span className="text-purple font-bold"> {movie}</span>
+          </span>
         </div>
         <span className="text-grey">{comment}</span>
       </div>
--- movie-picker-frontend/contexts/user.tsx
+++ movie-picker-frontend/contexts/user.tsx
@@ -1,26 +1,52 @@
 import React, { ReactNode } from 'react'
 import usersData from '../data/users.js'
+import ratingData from '../data/ratings.js'
+import reviewsData from '../data/reviews.js'
+import { Notification } from '../models/notification.js'
 import { User } from '../models/user.js'
 
 interface storeType {
   users: User[],
-  currentUser: User,
+  currentUser: User | null,
+  notifications: Notification[],
+  getUser: (userId: number) => User | undefined
 }
 
-export const StoreContext = React.createContext<storeType | null>(null)
+export const userContext = React.createContext<storeType>(
+  {
+    users: [],
+    currentUser: null,
+    notifications: [],
+    getUser: () => undefined,
+  }
+)
+
+const getNotifications = () : Notification[] => {
+  const items : Notification[] = []
+  Array.prototype.push.apply(items, reviewsData)
+  Array.prototype.push.apply(items, ratingData)
+  return items.sort((a: Notification, b: Notification) => { return (a.date < b.date) ? 1 : ((a.date > b.date) ? -1 : 0) })
+}
 
 export default ({ children } : { children: ReactNode }) => {
   const [users, setUsers] = React.useState(usersData)
   const [currentUser, setCurrentUser] = React.useState(usersData[0])
+  const [notifications, setNotifications] = React.useState(getNotifications())
+
+  const getUser = (userId: number): User | undefined => {
+    return users.find(user => user.id == userId)
+  }
 
   const store = {
     users,
     currentUser,
+    notifications,
+    getUser,
   }
 
   return (
-      <StoreContext.Provider value={store}>
+      <userContext.Provider value={store}>
         { children }
-      </StoreContext.Provider>
+      </userContext.Provider>
     )
   }
