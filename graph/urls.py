8e8063d48be192aa7fdaaa92d1b58de8c07e3d01
Nicolò Sonnino <swagpino@gmail.com>
2022-11-20 02:19:51 +0100

    Revert "feat: backend"
    This reverts commit 7cb4dd9e715a88f56dfd30b714967ba44ed3d44c.

--- graph/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('getMovie/', views.get_node, name='get_node'),
-    path('getAll/', views.get_all, name='get_all'),
-    # path('login/', views.login_view, name='login'),
-]
