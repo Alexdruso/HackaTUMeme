ff66c37a4cfa334a11d1f3f9a806de24d335f348
thuanystuart <thuanystuart@gmail.com>
2022-11-20 10:42:52 +0100

    frontend: add personalized feed

--- movie-picker-frontend/components/notifications/rated.tsx
+++ movie-picker-frontend/components/notifications/rated.tsx
@@ -1,12 +1,18 @@
+import { User } from "../../models/user"
 import UserImage from "../user-image"
 
-function RatedNotification() {
+function RatedNotification({ rating, movie, user }: { rating: number, movie: string, user: User | undefined}) {
   return (
     <div className="shadow rounded p-4 flex gap-4">
       <UserImage size="l" hover={false}></UserImage>
       <div>
-        <span className="text-purple font-bold">User name</span>
-        <span className="text-grey"> has rated a movie!</span>
+        <div>
+          <span className="text-purple font-bold">{user?.first_name}</span>
+          <span className="text-grey"> has rated the movie
+            <span className="text-purple font-bold"> {movie} </span>
+            with {rating} stars!
+          </span>
+        </div>
       </div>
     </div>
   )
--- movie-picker-frontend/models/rating.ts
+++
@@ -1,6 +0,0 @@
-export type Rating = {
-  movie: string,
-  user: number,
-  rating: number,
-  date: string,
-}
